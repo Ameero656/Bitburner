9df48479b148029c8b0c2078bc5642254dfcbd31
Ameero656 <62350131+Ameero656@users.noreply.github.com>
2023-05-22 16:35:19 -0500

    Update roboCopyScript.js

--- roboCopyScript.js
+++ roboCopyScript.js
@@ -1,63 +1,85 @@
-/** @param {NS} ns */
 export async function main(ns) {
-	const host = ns.args[0]; //where virus script will live
-	const target = ns.args[1]; //where virus script will attack
-	const maxMoney = ns.getServerMaxMoney(target);
+	let target = ns.args[0];
+	let greed = ns.args[1];
 
-	/*
-	hack time / hack thread = growtime / grow thread | growthtime *=growThread * hacktime/hackthread
-	*/
-	var growingTime = ns.getGrowTime(target);
-	var hackingTime = ns.getHackTime(target);
-	var weakeningTime = ns.getWeakenTime(target);
+	const spacer = 20; //20ms intervles between scripts
 
-	var hackThreadCount = Math.floor(0.5 / ns.hackAnalyze(target));
-	// var growThreadTimeRatio = Math.ceil(growingTime/9) * Math.ceil(hackingTime/9) / hackThreadCount;
-	var growThreadCount = Math.floor(ns.growthAnalyze(target, 2)); // growThreadTimeRatio);
-	var weakenThreadCount = Math.floor(hackThreadCount / 2); //(Hack threads / hackToWeakenRatio)
+	let hackThreads = Math.floor(greed / ns.hackAnalyze(target));
+	let weakenHackThreads = Math.ceil(hackThreads / 25;
+	let growThreads = Math.ceil(ns.growAnalyze(target, 2));
+	let weakenGrowThreads = Math.ceil(growThreads / 12.5);
 
-	const hackScript = 'virusHackScript.js';
-	const growScript = 'virusGrowScript.js';
-	const weakenScript = 'virusWeakenScript.js';
-
-	var totalThreadCount = hackThreadCount + growThreadCount + weakenThreadCount;
-	const maxThreadCount = ns.getServerMaxRam(host) / ns.getScriptRam(hackScript);
-	var universalRatio = Math.floor(maxThreadCount / totalThreadCount);
-
-	ns.tprint(`
-	maxMoney:${maxMoney}
-	growingTime:${growingTime}
-	hackingTime:${hackingTime}
-	weakeningTime:${weakeningTime}
-
-	hackThreadCount:${hackThreadCount}
-
-	growThreadCount:${growThreadCount}
-
-	weakenThreadCount:${weakenThreadCount}
-
-	totalThreadCount:${totalThreadCount}
-	maxThreadCount:${maxThreadCount}
-	universalRatio:${universalRatio}	
+	let hackDelay = ns.hackTime(target) - ns.growTime(target) + spacer; // 
+	let weakenHackDelay = spacer;
+	let growDelay = spacer;
+	let weakenGrowDelay = ns.weakenTime(target) - ns.growTime(target) + spacer;
 
 	
-	`)
-
-	var filenameArray = [hackScript, growScript, weakenScript];
-	var threadArray = [hackThreadCount, growThreadCount, weakenThreadCount];
-
-
-	for (let i = 0; i < filenameArray.length; i++) {
-		let script = filenameArray[i];
-		let threads = threadArray[i];
-		ns.tprint("Filename:", script, "| Threads:", threads)
-
-		let copied = ns.scp(script, host) //copy virus script to host
-		copied = copied == true ? 'sucessful!' : 'unsucessful.';
-		ns.tprint(`copying ${script}...${copied}`);
-
-		var executed = ns.exec(script, host, Math.floor(threads * universalRatio), target);
-		executed = executed > 1 ? "sucessful!" : 'unsucessful.';
-		ns.tprint(`executing ${script}...${executed}`);
+	const virusInfo = {
+		hack:  {
+			threads : hackThreads,
+			delay : hackDelay,
+			fileName : 'virusHack.js'
+		},
+		weakenHack: {
+			threads : weakenHackThreads,
+			delay : weakenHackDelay,
+			fileName : 'virusWeaken.js'
+		},
+		grow: {
+			threads : growThreads,
+			delay : growDelay,
+			fileName : 'virusGrow.js'
+		},
+		weakenGrow: {
+			threads : weakenGrowThreads,
+			delay : weakenGrowDelay,
+			fileName : 'virusWeaken.js'
+		}
 	}
+	let batches = Math.floor(ns.getServerMaxRam(target) / totalThreads * ns.getScriptRam(scripts[0]));
+
+	for (let i = 0; i < virusInfo.length; i++) {
+		for (let b = 0; b < batches; b++) {
+			let virusDelay = virusInfo[i][delay];
+			let virusThreads = virusInfo[i][threads];
+			let script = virusInfo[i][fileName];
+			let selectedServer = getAvalibleServers(ns, script);
+			plantVirus(ns, script, selectedServer, virusThreads[i], target, virusDelay, b);
+			executeVirus(ns, script, selectedServer);
+		}
+
+	}
+
+
+
+
+}
+
+function executeVirus(ns, script, server, threads, target, virusDelayArray, i, b) {
+	let executed = ns.exec(script, server, threads, target, virusDelayArray, b); // b is just id arg for multiple batches
+	executed = executed > 1 ? 'Sucessful!' : 'Unsucessful.';
+	ns.tprint(`Executing ${script} from ${server}...${executed} `)
+}
+
+function plantVirus(ns, script, selectedServer) {
+	let copied = ns.scp(script, selectedServer, 'home');
+	copied = copied ? 'Sucessful!' : 'Unsucessful.';
+	ns.tprint(`Copying ${script} to ${selectedServer}...${copied} `)
+	return;
+}
+
+
+function getAvalibleServers(ns, script) {
+	let skillServers = ['Black', 'Red', 'Home'];
+	let servers = ns.scan('home');
+	for (let i = 0; i < servers.length; i++) { }
+	let server = servers[i];
+	if (skillServers.includes(server.slie(0, 2))) {
+		continue
+	}
+	if (ns.getServerMaxRam(server) - ns.getServerUsedRam(server) >= ns.getScriptRam(script)) {
+		return server;
+	} else continue;
+}
 }
